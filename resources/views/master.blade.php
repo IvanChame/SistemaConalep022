1fb9891268ece725fe6a486e4a7a41561033e082
ivan <marroquin_ivan1@hotmail.com>
2025-12-07 21:45:12 -0600

    Primer commit de mi proyecto sistema conalep022

---
+++ resources/views/master.blade.php
@@ -0,0 +1,30 @@
+<script>
+if (!sessionStorage.getItem("logueado")) {
+    window.location.href = "login.html";
+}
+</script>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Visor de Excel</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+    <link rel="stylesheet" href="css/Estilo.css">
+
+</head>
+<body>
+
+<h2>Cargar Excel y mostrar contenido en pantalla</h2>
+
+<input type="file" id="archivoExcel" accept=".xlsx,.xls">
+<button onclick="mostrarExcel()">Mostrar Excel</button>
+
+
+<div id="contenedor"></div>
+
+<script src="js/Escript.js"></script>
+
+</body>
+</html>
